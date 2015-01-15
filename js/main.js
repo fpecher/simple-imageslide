2e036ba32dda8443420d3450ec895e9923fb986e
fpecher <pecherfabian@gmail.com>
2015-01-15 11:52:59 +0100

    removed senseless toggle

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 var main = function() {
-    $('.dropdown-toggle').click(function() {
-        $('.dropdown-menu').toggle();
-    });
-    
     $('.arrow-next').click(function() {
         var currentSlide = $('.active-slide');
         var nextSlide = currentSlide.next();
